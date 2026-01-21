b736ee5cfc66ab48f31ac7086019234b33411f9c
HollowBell <protego3784@gmail.com>
2026-01-21 17:00:51 +0900

    자신의 이름, 나이, 학과 출력하기

---
+++ MyInfoApp.java
@@ -0,0 +1,10 @@
+package study;
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		int n = 20;
+		System.out.print("Kitae Hwang\n");
+		System.out.print(n + " years old\n");
+		System.out.print("Department of Computer Engineering");
+	}
+}
